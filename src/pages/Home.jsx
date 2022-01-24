c65847216e52e6646b46af21bff48457b4dec1b6
Siddharth Arya <siddhartharya084@gmail.com>
2022-01-24 13:31:57 +0530

    added Newsletter section on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Anouncement from '../components/Anouncement'
 import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
 import Products from '../components/Products'
 import Slider from '../components/Slider'
-
+// import Footer from '../components/Footer'
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
+      {/* <Footer /> */}
     </div>
   )
 }
