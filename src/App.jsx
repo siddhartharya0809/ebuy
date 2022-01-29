151db8bf1abc366838242800093363a2c30c05d3
Siddharth Arya <siddhartharya084@gmail.com>
2022-01-29 18:57:48 +0530

    created product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import Home from "./pages/Home";
+import Product from "./pages/Product";
+// import Home from "./pages/Home";
+// import ProductList from "./pages/ProductList";
 
 const App = () => {
   return (
-    <Home />
+    // <Home />
+    // <ProductList />
+    <Product />
   )
 };
 
