48267462d0f8cf1ee91ae8e6e3b81422403e6b8a
Siddharth Arya <siddhartharya084@gmail.com>
2022-01-29 18:51:53 +0530

    made Product list page

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,86 @@
+
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Anouncement";
+import Products from "../components/Products";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
+
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 10px;
+
+`
+const Option = styled.option``
+
+function ProductList() {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Product:</FilterText>
+          <Select>
+            <Option defaultChecked > Color </Option>
+            <Option> White </Option>
+            <Option> Black </Option>
+            <Option> Red </Option>
+            <Option> Blue </Option>
+            <Option> Yellow </Option>
+            <Option> Green </Option>
+          </Select>
+          <Select>
+            <Option defaultChecked>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option defaultChecked>Featured</Option>
+            <Option>Discount - High to Low</Option>
+            <Option>Discount - Low to High</Option>
+            <Option>Price - High to Low</Option>
+            <Option>Price - Low to High</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+}
+
+export default ProductList;
