32c3234a8e51b88c64c9c30c743dd88896b9401f
Siddharth Arya <siddharhtarya084@gmail.com>
2022-02-05 01:25:40 +0530

    added React-router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 import Cart from "./pages/Cart";
-// import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import Product from "./pages/Product";
-// import Register from "./pages/Register";
-// import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="product-list" element={<ProductList />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </div>
     // <Home />
     // <ProductList />
     // <Product />
     // <Register />
     // <Login />
-    <Cart />
+    // <Cart />
   )
 };
 
